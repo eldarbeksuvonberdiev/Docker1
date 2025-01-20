b364f6c08c591349b6a8c9b9cf531be94caca78f
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2025-01-20 22:02:55 +0500

    Boshlandi vazifa

--- public/index.php
+++ public/index.php
@@ -1,11 +1,38 @@
+<?php
+
+session_start();
+if (isset($_GET['sum'])) {
+    $num = $_GET['sum'];
+    if ($num == 60) {
+        $_SESSION['page'] = 2;
+    } else {
+        echo "Noto'g'ri hisoblangan!";
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>test</h1>
+
+    <h2>Yig'indini hisoblang:</h2>
+
+    <h3>15 + 45 = </h3>
+
+    <form action="#" method="get">
+        <input type="text" name="sum">
+        <input type="submit" value="Send">
+    </form>
+
 </body>
+
 </html>
